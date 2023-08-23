34b72edb310a64cbb83b0da87fce505d22913007
Nikil-12345 <nikilkannan53@gmail.com>
2023-08-23 17:29:05 +0530

    Flask Project

---
+++ hello/app.py
@@ -0,0 +1,12 @@
+from doctest import debug
+from flask import Flask,render_template
+
+app=Flask(__name__,template_folder="templates")
+
+@app.route('/')
+@app.route('/nikil')
+def home():
+    return render_template('index.html');
+
+if __name__=='__main__':
+    app.run(debug=True)
